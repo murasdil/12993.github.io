9c39feebb79a15734c5aefbbee30ccfe2d3fdcb2
Murasdil <45697682+murasdil@users.noreply.github.com>
2021-02-05 22:52:14 +0600

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1612543488,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/background.png",
+		"images/custom-sheet0.png",
+		"images/lazer-sheet0.png",
+		"images/fire-sheet0.png",
+		"images/asteroid-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/explosion-sheet1.png",
+		"images/wrap-sheet0.png",
+		"images/warpenergy-sheet0.png",
+		"images/warpbg-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/shieldbonus-sheet0.png",
+		"images/shield-sheet0.png",
+		"images/congratulations-sheet0.png"
+	]
+}
